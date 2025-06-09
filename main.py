e39c1626c4db610054ae5c61de7685002f3805ac
Alan Gao <ygao9122@uni.sydney.edu.au>
2025-06-09 09:07:38 +0800

    fetch image

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_product_url():
 
 def get_soup(url):
     try:
-        res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0', "Referer": "https://www.focusproducts.com.au"})
         res.raise_for_status()
         return BeautifulSoup(res.text, 'html.parser')
     except requests.RequestException as e:
@@ -46,7 +46,7 @@ def fetch_product_info(soup):
         print(f"Error: {e}")
         return None
 
-def fetch_product_image(soup):
+def get_image_url(soup):
     try:
         image_element = soup.find('img', class_='CENy8b')
         if image_element and 'src' in image_element.attrs:
@@ -57,6 +57,30 @@ def fetch_product_image(soup):
     except Exception as e:
         print(f"Error fetching image: {e}")
         return ""
+
+def fetchProductImage(url, filename):
+    if not url or not filename:
+        print(f"Invalid URL or filename on {filename}")
+        return None
+    
+    headers = {
+        'User-Agent': 'Mozilla/5.0',
+        "Referer": "https://www.focusproducts.com.au"
+    }
+    try:
+        res = requests.get(url, headers=headers)
+        res.raise_for_status()
+        if res.status_code == 200:
+            with open(f"images/{filename}", 'wb') as f:
+                f.write(res.content)
+                print(f"{filename}图片下载成功")
+                return None
+        else:
+                print(f"{filename}图片下载失败")
+                return None
+    except Exception as e:
+        print(f"Error fetching {url}: {e}")
+        return None
 
 def main():
     BRAND = "testBrand"
@@ -77,14 +101,14 @@ def main():
         for product, url in zip(products_name, products_url):
             soup = get_soup(url)
             description = fetch_product_info(soup)
-            image = fetch_product_image(soup)
+            download_image = fetchProductImage(get_image_url(soup), f"{product}.jpg")
             if description:
                 writer.writerow({
                     "Name": product,
                     "Description": description,
                     "Category": CATAGORY,
                     "Brand": BRAND,
-                    "Image": image
+                    # "Image": image
                 })
             else:
                 print(f"Failed to fetch description for {product}")
